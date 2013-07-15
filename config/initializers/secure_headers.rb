432d483dfe0da10ad7e4fd11d0020d2e633a85d9
Matt Konda <mkonda@jemurai.com>
2013-07-14 21:17:28 -0500

    Add some problems back into the mix.  ;)

---
+++ config/initializers/secure_headers.rb
@@ -0,0 +1,14 @@
+::SecureHeaders::Configuration.configure do |config|
+  config.hsts = { :max_age => 99, :include_subdomains => true}
+  config.x_frame_options = 'DENY'
+  config.x_content_type_options = "nosniff"
+  config.x_xss_protection = {:value => 1, :mode => false}
+  config.csp = {
+    :default_src => "https://* inline eval",
+    :report_uri => '//chicagoruby.com/uri-directive', 
+    :img_src => "https://chicagoruby.com/* data:",
+    :frame_src => "https://* http://*.twimg.com"
+  }
+end
+
+# ensure_security_headers
