45877e20a7b62df9b7af4da77722776820b228d0
Matt Konda <mkonda@jemurai.com>
2013-09-11 22:26:11 -0500

    rspec examples for user model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.new
+    @user.email = "hi@hi.com"
+  end
+    
+  it "should not allow short passwords" do
+    @user.password = "hi1B"
+    @user.save
+    @user.errors.should have(1).messages
+    @user.errors.messages[:password].should eql ["is too short (minimum is 8 characters)"]
+  end
+
+  it "should not allow passwords without a digit" do
+    @user.password = "highthere"
+    @user.save
+    @user.errors.should have(1).messages
+    @user.errors.messages[:password].should eql ["must include at least one lowercase letter, one uppercase letter, and one digit"]
+  end
+  
+  it "should not allow passwords without an alpha" do
+    @user.password = "32434234324"
+    @user.save
+    @user.errors.should have(1).messages
+    @user.errors.messages[:password].should eql ["must include at least one lowercase letter, one uppercase letter, and one digit"]
+  end
+
+  it "should accept complex passwords with a lower, upper and digit" do
+      @user.password = "Passw0rd!"  
+      @user.save
+      @user.errors.should be_empty
+  end
+  
 end
